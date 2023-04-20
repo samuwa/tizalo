2eb109130a475e07202a6e2b9f4c113c71e011a0
samuwa <69830704+samuwa@users.noreply.github.com>
2023-04-20 12:37:59 -0500

    Create Cotiza.py

---
+++ Cotiza.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Cotiza :mechanical_arm:")
+
+st.markdown('''
+
+MVP descubrimiento y organizador de cotizaciones.
+
+    Diagnosis: Encontrar posibles soluciones a un problema y entender los paramentros necesarios para cotizar cada solución.
+    
+    Proveedores: Encontrar proveedores relevantes para cada solución.
+
+''')
