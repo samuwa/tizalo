0a581e2f6ed85ac2b350e4459b1180882137910d
samuwa <69830704+samuwa@users.noreply.github.com>
2023-04-21 11:14:27 -0500

    Update Cotiza.py

--- Cotiza.py
+++ Cotiza.py
@@ -1,20 +1,6 @@
 import streamlit as st
 
 
-st.set_page_config(
-    page_title="Cotiza :mechanical_arm:",
-    page_icon=":mechanical_arm:",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        "Get Help": None,
-        "Report a bug": "https://github.com/streamlit/streamlit/issues/new/choose"
-    },
-    metadata={
-        "google-site-verification": "google6ac22868640d7e2c.html"
-    }
-)
-
 st.title("Cotiza :mechanical_arm:")
 
 st.markdown('''
