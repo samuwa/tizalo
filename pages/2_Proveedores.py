6154c60f1b8fc2541df2bac82091b6c58dc705db
samuwa <69830704+samuwa@users.noreply.github.com>
2023-04-21 10:44:49 -0500

    Update 2_Proveedores.py

--- pages/2_Proveedores.py
+++ pages/2_Proveedores.py
@@ -11,21 +11,6 @@ st.title(":mag: Proveedores")
 google_api = func.credenciales_google()
 # Categorías:
 
-categorias = ['Electricista','Plomero','Pintura','Iluminación','Gypsero','Ebanista','Mobiliario','Aires Acondicionados','Decoradores',
-              'Co-workings','Papel de pared','Cerrajero','Pintura Industrial','Instalador','Sistema de Bombeo','Albañil','Cielo Rasos','Soldadura',
-              'Techos','Ventanas','Vidrio','Tapicería','Fumigación','Pulido de pisos','Limpieza interna','Lavandería','Limpieza de ventanas',
-              'Limpieza de alfombras','Recolección de Basura','Recolección de Reciclaje','Guindolas','Andamios','Plataformas Elevadas','Vigilancia',
-              'Cámaras de seguridad','Control de acceso','Background Checks','Ciberseguridad','Sistemas de alarmas','Logística terreste',
-              'Logistica maritima','Talleres','Servicios de mensajeria','Repuestos para autos','Sensores de incendio','Fugas de gas','Estudios de impacto ambiental',
-              'Estudio de suelo','Event Planners','Entretenimiento','Reservaciones','Almuerzos ejecutivos','Catering','Eventos deportivos','Floristería',
-              'Proveedor de Wifi','Red telefonica','Agencias de viaje','Planes de turismo','Alquiler de yates','Seguro de responsabilidad civil',
-              'Seguros de vida','Seguros de salud privado','Seguros de vehiculos','Seguros contra robos','Servicios de traducción','Call Centers',
-              'Staff On Demand','Head Hunters','Digitalización de documentos','Auditoría','Servicio de impresión','Servicio de notaría','Contabilidad',
-              'Servicios legales','Laboratorios médicos','Campañas de mercadeo','Membresías de gimnasio/ Reservaciones de canchas',
-              'Autolavados','Clases particulares para niños','Guarderias','Recursos Humanos','Pasarelas de pago',
-              'Consultores de nube','Soporte técnico','Útiles de oficina','Articulos de limpieza','Snacks','Bebidas alcoholicas',
-              'Desechables','Dispensadores de agua',
-              'Tanques de gas','Linea Blanca','Uniformes','Bordados','Frutas y Verduras', "fontanero"]
 
 categorias = categorias.sort()
 
