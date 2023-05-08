1a11b1d64453a7dad30e0c01019073ff8cd65811
samuwa <69830704+samuwa@users.noreply.github.com>
2023-05-08 11:31:38 -0500

    Update 3_Recotizar.py

--- pages/3_Recotizar.py
+++ pages/3_Recotizar.py
@@ -7,4 +7,4 @@ st.title(":mag: Recotizar")
 uploaded = st.file_uploader("Agregar cotizacion existente")
 
 if uploaded != None:
-    st.write(func.summarize_pdf(st.secrets[GPT_API],uploaded))
+    st.write(func.summarize_pdf(st.secrets["GPT_API"],uploaded))
