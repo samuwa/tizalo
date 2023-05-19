df73c46b1441040ef9ffb3aa31086d9e74373193
samuwa <69830704+samuwa@users.noreply.github.com>
2023-05-18 22:29:42 -0500

    Create 4_Diagnosis Chat.py

---
+++ pages/4_Diagnosis Chat.py
@@ -0,0 +1,85 @@
+import openai
+import streamlit as st
+from streamlit_chat import message
+
+# Setting page title and header
+
+st.title("Diagnosis 2.0 :female-scientist:")
+
+st.info("**Mensaje ayuda:** Mi cliente necesita XXX, a que profesional o tipo de empresa debo contactar y que información debo darle?")
+
+# Set org ID and API key
+openai.api_key = "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"
+
+# Initialise session state variables
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+if 'model_name' not in st.session_state:
+    st.session_state['model_name'] = []
+if 'total_tokens' not in st.session_state:
+    st.session_state['total_tokens'] = []
+
+
+clear_button = st.button("Clear Conversation", key="clear")
+
+# Map model names to OpenAI model IDs
+model = "gpt-3.5-turbo"
+
+# reset everything
+if clear_button:
+    st.session_state['generated'] = []
+    st.session_state['past'] = []
+    st.session_state['messages'] = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+    st.session_state['number_tokens'] = []
+    st.session_state['total_tokens'] = []
+
+# generate a response
+def generate_response(prompt):
+    st.session_state['messages'].append({"role": "user", "content": prompt})
+
+    completion = openai.ChatCompletion.create(
+        model=model,
+        messages=st.session_state['messages']
+    )
+    response = completion.choices[0].message.content
+    st.session_state['messages'].append({"role": "assistant", "content": response})
+
+    # print(st.session_state['messages'])
+    total_tokens = completion.usage.total_tokens
+    prompt_tokens = completion.usage.prompt_tokens
+    completion_tokens = completion.usage.completion_tokens
+    return response, total_tokens, prompt_tokens, completion_tokens
+
+
+# container for chat history
+response_container = st.container()
+# container for text box
+container = st.container()
+
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_area("You:", key='input', height=100)
+        submit_button = st.form_submit_button(label='Send')
+
+    if submit_button and user_input:
+        output, total_tokens, prompt_tokens, completion_tokens = generate_response(user_input)
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
+        st.session_state['total_tokens'].append(total_tokens)
+
+        # from https://openai.com/pricing#language-models
+
+
+if st.session_state['generated']:
+    with response_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
