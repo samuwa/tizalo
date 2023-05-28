88883f3e3eceedf3af9c0377ee57068c5ac75f43
samuwa <69830704+samuwa@users.noreply.github.com>
2023-05-28 13:20:44 -0500

    Update 2_Proveedores.py
    Agregar paginas de busqueda

--- pages/2_Proveedores.py
+++ pages/2_Proveedores.py
@@ -27,7 +27,22 @@ categoria = bform.text_input("Categoría")
 boton = bform.form_submit_button("Buscar")
 
 if boton:
-    provs = func.get_places(google_api, categoria, ubicacion, radio)
+    provs = func.get_places(api_key=google_api, location=ubicacion, radius=radio, category=categoria, region="pa")
+    token = provs[1]
+    provs = provs[0]
+
+    if token != None:
+        alist = func.get_places(api_key=google_api, location=ubicacion, radius=radio, category=categoria, region="pa", token=token)
+        for x in alist[0]:
+            provs.append(x)
+
+        atoken = alist[1]
+        if atoken is not None and type(atoken) == str:
+            blist = func.get_places(api_key=google_api, location=ubicacion, radius=radio, category=categoria, region="pa", token=atoken)
+            for x in blist[0]:
+                provs.append(x)
+
+    provs = func.sort_dicts_by_keys(provs, ['puntaje', 'user_ratings_total'])
     numeros = []
 
     for x in provs:
