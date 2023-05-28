364722056603643c56f62bd08efa4390462b3948
samuwa <69830704+samuwa@users.noreply.github.com>
2023-05-28 18:26:54 -0500

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -9,7 +9,6 @@ import warnings
 import pandas as pd
 import re
 
-# Next page session state:
 
 # Credenciales
 
@@ -93,9 +92,7 @@ def get_places(api_key, category, location, radius, region, token=None):
     places = gmaps.places(query=category, location=location, radius=radius, region=region, page_token=token)
 
     next_page = places.get('next_page_token', None)
-    if 'next_page' not in st.session_state:
-        st.session_state['next_page'] = None
-    st.session_state['next_page'] = next_page
+
     st.write(next_page)
 
     # Clean first level dictionary
