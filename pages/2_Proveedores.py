8f7bf14cdea05143cd02c55d774e1cbe33b69e55
samuwa <69830704+samuwa@users.noreply.github.com>
2023-05-28 13:50:40 -0500

    Update 2_Proveedores.py

--- pages/2_Proveedores.py
+++ pages/2_Proveedores.py
@@ -31,21 +31,14 @@ if boton:
     token = provs[1]
     provs = provs[0]
     
-    st.write(type(token))
-
-    if type(token) is not str:
-        pass
-    else:
-        try:
-            alist = func.get_places(api_key=google_api, location=ubicacion, radius=radio, category=categoria, region="pa", token=token)
-            for x in alist[0]:
-                provs.append(x)
-         except:
-            pass
+#     if token != None:
+#         alist = func2.get_places(api_key=google_api, location=ubicacion, radius=radio, category=categoria, region="pa", token=token)
+#         for x in alist[0]:
+#             provs.append(x)
 
 #         atoken = alist[1]
 #         if atoken is not None and type(atoken) == str:
-#             blist = func.get_places(api_key=google_api, location=ubicacion, radius=radio, category=categoria, region="pa", token=atoken)
+#             blist = func2.get_places(api_key=google_api, location=ubicacion, radius=radio, category=categoria, region="pa", token=atoken)
 #             for x in blist[0]:
 #                 provs.append(x)
      
