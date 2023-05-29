1bc0945cb76f659a9039a415baf8f0c6b1a75ac6
samuwa <69830704+samuwa@users.noreply.github.com>
2023-05-28 19:30:45 -0500

    Update 2_Proveedores.py

--- pages/2_Proveedores.py
+++ pages/2_Proveedores.py
@@ -54,7 +54,7 @@ if boton:
             for x in blist[0]:
                 provs.append(x)
                 
-     for key in st.session_state.keys():
+    for key in st.session_state.keys():
         del st.session_state[key]
      
 
