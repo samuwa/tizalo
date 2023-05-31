e1163fe949a7fca44b3f513211c8e680deeca873
samuwa <69830704+samuwa@users.noreply.github.com>
2023-05-31 11:20:05 -0500

    Update 1_Diagnosis.py

--- pages/1_Diagnosis.py
+++ pages/1_Diagnosis.py
@@ -12,6 +12,7 @@ st.title(":female-scientist: Diagnosis")
 aform = st.form("F1")
 
 mensaje = aform.text_input("Qué necesita el cliente?")
+st.info("Solamente indique que necesita el cliente. Ej: Plomeria, Utiles escolares, transporte terrestre, etc.")
 
 boton = aform.form_submit_button("Preguntar")
 
