3073c315a804efec1b0b57aadcbde727dc14164c
samuwa <69830704+samuwa@users.noreply.github.com>
2023-09-15 08:49:09 -0500

    Update 2_Proveedores.py

--- pages/2_Proveedores.py
+++ pages/2_Proveedores.py
@@ -149,8 +149,8 @@ if 'provs' in st.session_state:
             nombres.append(x['name'])
 
     if numeros and nombres:
-        st.download_button(label="**Descargar números WhatsApp**", data=func.create_csv(numeros, nombres), file_name='numeros_proveedores.csv', mime='text/csv')
-        st.download_button(label="**Descargar números Celulares**", data=func.create_csv_10(numeros, nombres), file_name='numeros_proveedores.csv', mime='text/csv')
+        st.download_button(label="**Descargar números WhatsApp**", data=func.create_csv(numeros, nombres), file_name='numeros_proveedores_WhatsApp.csv', mime='text/csv')
+        st.download_button(label="**Descargar números Fijos**", data=func.create_csv_10(numeros, nombres), file_name='numeros_proveedores_Fijos.csv', mime='text/csv')
     
     for x in st.session_state['provs']:
        # Displaying each provider's details in the streamlit app
